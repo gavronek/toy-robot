2e3fefe16c30ffd0c54aea1b1e79155d8344fa8f
Adam Gavronek <adam.gavronek@zalando.de>
2018-02-17 17:03:29 +0100

    Initialize SpringBoot application

---
+++ src/main/java/com/gavronek/toyrobot/ToyrobotApplication.java
@@ -0,0 +1,12 @@
+package com.gavronek.toyrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyrobotApplication.class, args);
+	}
+}
